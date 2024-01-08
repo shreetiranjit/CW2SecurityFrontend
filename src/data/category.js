4f3ef001bf91b3e3964f534452cb4301d00fbb8b
shreetiranjit <ranjit.shreeti1@gmail.com>
2024-01-08 21:34:17 +0545

    added data for the countries and categories

---
+++ src/data/category.js
@@ -0,0 +1,23 @@
+export const categories = [
+  'Men Fashion',
+  'Women Fashion',
+  'Baby',
+  'Electronic',
+  'Computers',
+  'Automotive',
+  'Sports & Outdoors',
+  'Homes & Kitchen',
+  'Luggage'
+];
+
+export const subCategories = [
+  ['Clothing', 'Shoes', 'Watches', 'Accessories'],
+  ['Clothing', 'Shoes', 'Jewelry', 'Handbags', 'Watches', 'Accessories'],
+  ['Baby Care', 'Diapering', 'Feeding', 'Activity & Entertainment', 'Toys', 'Accessories'],
+  ['Cell Phones & Accessories', 'TV & Video', 'Tablets', 'Accessories'],
+  ['Computer Components', 'Laptops', 'Monitors', 'Accessories'],
+  ['Car', 'Bike Accessories', 'Car Accessories', 'Bike'],
+  ['Sports & Outdoors', 'Sports & Fitness', 'Fan Shop'],
+  ['Home & Kitchen', 'Home Decor', 'Home Improvement', 'Home Appliances', 'Home Furnishings'],
+  ['Backpacks', 'Luggage Sets', 'Laptop Bags', 'Travel Accessories']
+];
